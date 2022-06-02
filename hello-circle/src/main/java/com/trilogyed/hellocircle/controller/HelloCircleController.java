0242700c241ed1b8cc26dea58737e694ccc51dcb
Andy <awilliamscomposing@gmail.com>
2022-06-02 07:49:35 -0700

    Fixed broken test

--- hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
+++ hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
@@ -10,4 +10,7 @@ public class HelloCircleController {
     public String helloCircle(){
         return "Hello, Circle!";
     }
+
+    @GetMapping(value = "/answer")
+    public String getMeaningOfLifeTheUniverseAndEverything() { return "The answer is 42!";}
 }
